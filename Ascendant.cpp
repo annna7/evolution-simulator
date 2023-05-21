32cb977b8315ca9ff811882f9d3c2e74eb98b70a
Anna Pecheanu <annapecheanu1@gmail.com>
2023-05-21 12:10:18 +0300

    Moved Ascendant logic into .cpp file

--- Ascendant.cpp
+++ Ascendant.cpp
@@ -1,5 +1,50 @@
-//
-// Created by anna on 5/14/23.
-//
 
 #include "Ascendant.h"
+
+
+Ascendant::Ascendant(int x, int y) : Individual(x, y) {
+    hasEaten = false;
+}
+
+sf::Color Ascendant::getColor() {
+    return sf::Color::Magenta;
+}
+
+int Ascendant::getHunger() const {
+    return 2;
+}
+
+void Ascendant::eat() {
+    if (!hasEaten) {
+        hasEaten = true;
+    }
+    Individual::eat();
+}
+
+int Ascendant::getVision() const {
+    if (hasEaten) {
+        return 10;
+    } else {
+        return Individual::getVision();
+    }
+}
+
+int Ascendant::getSpeed() const {
+    if (hasEaten) {
+        return 5;
+    } else {
+        return Individual::getSpeed();
+    }
+}
+
+Ascendant::Ascendant(const Ascendant &other) : Individual(other) {
+    hasEaten = other.hasEaten;
+}
+
+Ascendant& Ascendant::operator=(const Ascendant &other) {
+    if (this != &other) {
+        Individual::operator=(other);
+        hasEaten = other.hasEaten;
+    }
+    return *this;
+}
--- Ascendant.h
+++ Ascendant.h
@@ -12,44 +12,15 @@ class Ascendant : public Individual {
     bool hasEaten;
 
 public:
-    Ascendant(int x, int y) : Individual(x, y) {
-        hasEaten = false;
-    }
-    [[nodiscard]] sf::Color getColor() override { return sf::Color::Magenta; }
-    [[nodiscard]] int getHunger() const override { return 2; }
-    void eat() override {
-        if (!hasEaten) {
-            hasEaten = true;
-        }
-        Individual::eat();
-    }
-    [[nodiscard]] int getVision() const override {
-        if (hasEaten) {
-            return 10;
-        } else {
-            return Individual::getVision();
-        }
-    }
-    [[nodiscard]] int getSpeed() const override {
-        if (hasEaten) {
-            return 5;
-        } else {
-            return Individual::getSpeed();
-        }
-    }
-    // override copy constructor, assignment operator, swap
-    Ascendant(const Ascendant &other) : Individual(other) {
-        hasEaten = other.hasEaten;
-    }
-
-    Ascendant& operator=(const Ascendant &other) {
-        if (this != &other) {
-            Individual::operator=(other);
-            hasEaten = other.hasEaten;
-        }
-        return *this;
-    }
-
+    Ascendant(int x, int y);
+    [[nodiscard]] sf::Color getColor() override;
+    [[nodiscard]] int getHunger() const override;
+    void eat() override;
+    [[nodiscard]] int getVision() const override;
+    [[nodiscard]] int getSpeed() const override;
+    // override copy constructor, assignment operator
+    Ascendant(const Ascendant &other);
+    Ascendant& operator=(const Ascendant &other);
 };
 
 
